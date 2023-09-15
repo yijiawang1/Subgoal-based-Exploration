7469aa554dc4b552812ba204df5d8dda23201d46
yijiawang <38433580+yijiawang1@users.noreply.github.com>
2023-09-15 11:20:51 -0700

    Remove MAML

--- maml-rl-pytorch/bash_it10.sh
+++
@@ -1,5 +0,0 @@
-for i in {0..49}
-do
-  echo "IT, Looping ... number $i"
-  python train.py --config configs/maml/it10_4.yaml --output-folder results_it10_4 --seed "$i" --num-workers 8 
-done
--- maml-rl-pytorch/bash_ky.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-
-for i in {0..49}
-do
-  echo "Looping ... number $i"
-  python train.py --config configs/maml/ky_2.yaml --output-folder results_ky10One_2 --seed "$i" --num-workers 8 
-done
